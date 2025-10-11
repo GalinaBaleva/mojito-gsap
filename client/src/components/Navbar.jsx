778d9def3b0d03f14f9af4aa8ed191be93d6f1ba
Galina <baleva1990@gmail.com>
2025-10-11 15:29:58 +0200

    Animate Navbar.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,24 @@
 import {navLinks} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Navbar = () => {
+
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power2.inOut',
+        });
+    })
     return (
         <nav>
             <div className="navbar">
