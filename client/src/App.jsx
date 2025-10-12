710c92bf9e013324fbc3620ba514f9309a1e7c07
Galina <baleva1990@gmail.com>
2025-10-12 23:15:53 +0200

    Add Hero component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
 
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +10,7 @@ const App = () => {
     return (
         <>
             <Navbar />
+            <Hero />
 
         </>
     )
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+function Hero() {
+    return (
+        <>
+        <section id="hero" className="noisy">
+            <h1 className='title'>MOJITO</h1>
+
+            <img
+                src="/images/hero-left-leaf.png"
+                alt="right-leaf"
+                className='feft-leaf'
+            />
+            <img
+                src="/images/hero-right-leaf.png"
+                alt="right-leaf"
+                className='right-leaf'
+            />
+        </section>
+        </>
+    );
+}
+
+export default Hero;
