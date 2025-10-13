457cc0a2d19c8a71be5099c870c70ca7d7f89b4e
Galina <baleva1990@gmail.com>
2025-10-13 14:36:34 +0200

    Add animation to Hero background elements(Cocktail Glass).

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import {useGSAP} from "@gsap/react";
 import {SplitText} from "gsap/all";
 import gsap from "gsap";
+import {useRef} from "react";
+import {useMediaQuery} from "react-responsive";
 
 function Hero() {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -23,7 +27,37 @@ function Hero() {
             ease: 'expo.out',
             stagger: 0.06,
             delay: 1,
+        });
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
         })
+            .to('.right-leaf', {y: 200}, 0)
+            .to('.left-leaf', {y: -200}, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
 
     }, [])
     return (
@@ -34,7 +68,7 @@ function Hero() {
                 <img
                     src="/images/hero-left-leaf.png"
                     alt="right-leaf"
-                    className='feft-leaf'
+                    className='left-leaf'
                 />
                 <img
                     src="/images/hero-right-leaf.png"
@@ -60,6 +94,14 @@ function Hero() {
                 </div>
 
             </section>
+            <div className='video absolute inset-0'>
+                <video ref={videoRef}
+                src="/videos/output.mp4"
+                muted
+                playsInline
+                preload="auto"
+                />
+            </div>
         </>
     );
 }
