cd07de3b0222b2225b5f5e917d9daf2e835d1c6d
Galina <baleva1990@gmail.com>
2025-10-19 00:29:17 +0200

    Implement the Contact section animated css.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,34 @@
+import {openingHours, socials} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
+import {SplitText} from "gsap/all";
+import gsap from "gsap";
+
 function Contact() {
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#contact h2', { type: 'words'});
+
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#contact',
+                start: 'top center',
+            },
+            ease: 'power1.inOut'
+        });
+
+        timeline
+            .from(titleSplit.words, {
+                opacity: 0, yPercent: 100, stagger: 0.2,
+            })
+            .from('#contact h3 , #contact p', {
+                opacity: 0, yPercent: 100, stagger: 0.2,
+            })
+            .to('#f-right-leaf',{
+                y: '-50', duration: 1, ease: 'power1.inOut',
+            })
+            .to('#f-left-leaf',{
+                y: '-50', duration: 1, ease: 'power1.inOut',
+            }, '<');
+    })
     return (
         <footer id="contact">
             <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf"/>
@@ -6,6 +36,43 @@ function Contact() {
 
             <div className="content">
                 <h2>Where to Find Us</h2>
+
+                <div>
+                    <h3>Visit Our Bar</h3>
+                    <p>456, Raq Bld, Los Angeles, CA 905789</p>
+                </div>
+
+                <div>
+                    <h3>Contact Us</h3>
+                    <p>(555) 934-9823</p>
+                    <p>hello@jscocktail.com</p>
+                </div>
+
+                <div>
+                    <h3>Open Every Day</h3>
+                    {
+                        openingHours.map((time) => (
+                            <p key={time.day}>
+                                {time.day} : {time.time}
+                            </p>
+                        ))
+                    }
+                </div>
+
+                <div className="flex-center gap-5">
+                    ` {socials.map((social) => (
+                    <a
+                        key={social.name}
+                        href={social.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={social.name}
+                    >
+                        <img src={social.icon} alt="icon"/>
+                    </a>
+
+                ))}
+                </div>
             </div>
         </footer>
     );
